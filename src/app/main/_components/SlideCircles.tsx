a6b920fbbea90b2f6ce0398997df9b9ba53388d8
MaetDol <rndka9@gmail.com>
2025-01-27 18:31:22 +0900

    refactor, 별도 컴포넌트로 분리

---
+++ src/app/main/_components/SlideCircles.tsx
@@ -0,0 +1,68 @@
+import { useDragHandler } from "@/shared/hooks/useDragHandler";
+import { useState } from "react";
+
+interface Props {
+  circles: {
+    color: string;
+    name: string;
+    id: number;
+  }[];
+  onChange?: (idx: number) => void;
+}
+
+export function SlideCircles({ circles, onChange }: Props) {
+  const [idx, setIdx] = useState(0);
+  const [translateX, setTranslateX] = useState(0);
+
+  const updateIdx = (i: number) => {
+    setIdx(i);
+    onChange?.(i);
+  };
+
+  const { dragHandlers, isDragging } = useDragHandler({
+    onDragging: (xMove) => {
+      setTranslateX((prev) => prev + xMove);
+    },
+    onDragStop: () => {
+      const i = idx + Math.round(-translateX / 48);
+      setTranslateX(0);
+
+      if (i < 0) {
+        updateIdx(0);
+        return;
+      }
+
+      if (i >= circles.length) {
+        updateIdx(circles.length - 1);
+        return;
+      }
+
+      updateIdx(i);
+    },
+  });
+
+  const currentX = 12 - idx * 48;
+
+  return (
+    <div className="w-[64px] overflow-hidden relative" {...dragHandlers}>
+      <div className="absolute h-full left-0 top-0 shadow-[0_0_16px_2px] shadow-white z-1" />
+      <div className="absolute h-full right-0 top-0 shadow-[0_0_16px_2px] shadow-white z-1" />
+      <div
+        className="flex gap-2 w-fit transition-transform"
+        style={{
+          transform: isDragging
+            ? `translateX(${currentX + translateX}px)`
+            : `translateX(${currentX}px)`,
+        }}
+      >
+        {circles.map((circle, i) => (
+          <div
+            key={circle.id}
+            style={{ transform: idx !== i ? "scale(0.95)" : "" }}
+            className={`rounded-full w-[40px] h-[40px] transition-transform ${circle.color}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { SlideCircles } from "@/app/main/_components/SlideCircles";
 import { useDragHandler } from "@/shared/hooks/useDragHandler";
 import { useState } from "react";
 
@@ -37,56 +38,9 @@ const Emotions = [
 ];
 
 export default function Page() {
-  const [idx, setIdx] = useState(0);
-
-  const [translateX, setTranslateX] = useState(0);
-
-  const { dragHandlers, isDragging } = useDragHandler({
-    onDragging: (xMove) => {
-      setTranslateX((prev) => prev + xMove);
-    },
-    onDragStop: () => {
-      const i = idx + Math.round(-translateX / 48);
-      setTranslateX(0);
-
-      if (i < 0) {
-        setIdx(0);
-        return;
-      }
-
-      if (i >= Emotions.length) {
-        setIdx(Emotions.length - 1);
-        return;
-      }
-
-      setIdx(i);
-    },
-  });
-
-  const currentX = 12 - idx * 48;
-
   return (
     <main>
-      <div className="w-[64px] overflow-hidden relative" {...dragHandlers}>
-        <div className="absolute h-full left-0 top-0 shadow-[0_0_16px_2px] shadow-white z-1" />
-        <div className="absolute h-full right-0 top-0 shadow-[0_0_16px_2px] shadow-white z-1" />
-        <div
-          className="flex gap-2 w-fit transition-transform"
-          style={{
-            transform: isDragging
-              ? `translateX(${currentX + translateX}px)`
-              : `translateX(${currentX}px)`,
-          }}
-        >
-          {Emotions.map((emotion, i) => (
-            <div
-              key={emotion.id}
-              style={{ transform: idx !== i ? "scale(0.95)" : "" }}
-              className={`rounded-full w-[40px] h-[40px] transition-transform ${emotion.color}`}
-            />
-          ))}
-        </div>
-      </div>
+      <SlideCircles circles={Emotions} />
     </main>
   );
 }
