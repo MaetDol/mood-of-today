dbee939fd70f15964a41af89e607900beacdc29d
MaetDol <rndka9@gmail.com>
2025-01-27 22:28:02 +0900

    bugfix, 1일이 일요일부터 시작하면 두 달치 날짜가 나오는 이슈 수정

--- src/shared/utils/getDates.ts
+++ src/shared/utils/getDates.ts
@@ -5,18 +5,20 @@ export function getDates(year: number, month: number) {
 
   // 지난 달의 마지막 주 일요일부터 이번 달의 첫째 날 직전까지 가져온다
   // 28 29 30 31 1 2 3 처럼 보이게 하기 위해서!
-  const startDateOfPrevMonth = new Date(year, month - 1, 1 - firstDay);
-  const endDateOfPrevMonth = new Date(year, month - 1, 0);
-  for (
-    let i = startDateOfPrevMonth.getDate();
-    i <= endDateOfPrevMonth.getDate();
-    i++
-  ) {
-    const date = new Date(year, month - 2, i);
-    dates.push({
-      month: date.getMonth() + 1,
-      date: date.getDate(),
-    });
+  if (firstDay !== 0) {
+    const startDateOfPrevMonth = new Date(year, month - 1, 1 - firstDay);
+    const endDateOfPrevMonth = new Date(year, month - 1, 0);
+    for (
+      let i = startDateOfPrevMonth.getDate();
+      i <= endDateOfPrevMonth.getDate();
+      i++
+    ) {
+      const date = new Date(year, month - 2, i);
+      dates.push({
+        month: date.getMonth() + 1,
+        date: date.getDate(),
+      });
+    }
   }
 
   for (let i = 1; i <= lastDate.getDate(); i++) {
