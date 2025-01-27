3c2f8b9287d5542183e715159eccf9ac9941cc15
MaetDol <rndka9@gmail.com>
2025-01-27 20:49:25 +0900

    feat, 달력.. 비슷한걸 그리게 수정

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -2,7 +2,8 @@
 
 import { SlideCircles } from "@/app/main/_components/SlideCircles";
 import { useDragHandler } from "@/shared/hooks/useDragHandler";
-import { useState } from "react";
+import { getDates } from "@/shared/utils/getDates";
+import { useEffect, useState } from "react";
 
 const Emotions = [
   {
@@ -38,8 +39,33 @@ const Emotions = [
 ];
 
 export default function Page() {
+  const [calendar, setCalendar] = useState({
+    year: new Date().getFullYear(),
+    month: new Date().getMonth() + 1,
+  });
+
+  const prevMonth = () => {
+    const { year, month } = calendar;
+    if (month === 1) {
+      setCalendar({ year: year - 1, month: 12 });
+      return;
+    }
+
+    setCalendar({ year, month: month - 1 });
+  };
+
+  const nextMonth = () => {
+    const { year, month } = calendar;
+    if (month === 12) {
+      setCalendar({ year: year + 1, month: 1 });
+      return;
+    }
+
+    setCalendar({ year, month: month + 1 });
+  };
+
   return (
-    <main className="bg-white px-5 py-6 flex flex-col">
+    <main className="bg-white px-5 py-6 flex flex-col flex-1">
       <div className="flex-1 shrink-0 flex flex-col gap-3">
         <div className="flex items-center gap-4 shrink-0">
           <SlideCircles circles={Emotions} />
@@ -60,7 +86,27 @@ export default function Page() {
         </ol>
       </div>
 
-      <div></div>
+      <div
+        className="mt-auto grid grid-cols-[repeat(7,16px)] 
+      justify-center justify-items-center gap-x-6"
+      >
+        <div className="text-red-600 mb-4">일</div>
+        <div>월</div>
+        <div>화</div>
+        <div>수</div>
+        <div>목</div>
+        <div>금</div>
+        <div className="text-blue-600">토</div>
+
+        {getDates(calendar.year, calendar.month).map(({ month, date }) => (
+          <div
+            key={month.toString() + date}
+            className={month !== calendar.month ? "opacity-20" : ""}
+          >
+            {date}
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
