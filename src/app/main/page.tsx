da886e0702b00c4e82ee9af59f689992d1eae11e
MaetDol <rndka9@gmail.com>
2025-01-28 10:01:51 +0900

    feat, 입력창 핸들링 임시 추가

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -2,6 +2,7 @@
 
 import { Calendar } from "@/app/main/_components/Calendar";
 import { SlideCircles } from "@/app/main/_components/SlideCircles";
+import { useRef, useState } from "react";
 
 // 색깔 다시 정할 필요가 있음
 const Emotions = [
@@ -35,14 +36,40 @@ const Emotions = [
     name: "불안함",
     id: 6,
   },
-];
+] 
+
+type Mood = {
+  id: number;
+  name: string;
+  moodId: number;
+}
 
 export default function Page() {
+
+  const moodRef = useRef<typeof Emotions[number]>(null);
+  const [moods, setMoods] = useState<Mood[]>([]);
+  const handleInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if(e.key === 'Enter') {
+      setMoods((prev) => [
+        ...prev,
+        {
+          id: Date.now(),
+          name: e.target.value as any,
+          moodId: moodRef.current.id,
+        },
+      ]);
+      e.target.value = '';
+  }
+
+  const handleChangeMood = (idx: number) => {
+    moodRef.current = Emotions[idx];
+  }
+
   return (
     <main className="bg-white px-5 py-6 flex flex-col flex-1">
       <div className="flex-1 shrink-0 flex flex-col gap-3">
         <div className="flex items-center gap-4 shrink-0">
-          <SlideCircles circles={Emotions} />
+          <SlideCircles circles={Emotions} onChange={handleChangeMood} />
           <input
             className="px-3 py-2 rounded-full border border-slate-400
           w-[200px]
