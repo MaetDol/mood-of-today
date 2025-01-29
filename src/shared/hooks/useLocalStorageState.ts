097478b4b44bcb9ed6f72291cc2033a81093b66c
MaetDol <rndka9@gmail.com>
2025-01-29 11:15:15 +0900

    feat, 초기화시 매핑하는 함수 추가할 수 있게 수정
    nested object 의 경우 잘 파싱하지 못할때가 있어 해당 케이스를 위해 추가했어요 (date 같은 애들)

--- src/shared/hooks/useLocalStorageState.ts
+++ src/shared/hooks/useLocalStorageState.ts
@@ -3,15 +3,26 @@
 import { localStorageService } from "@/shared/services/localStorageService";
 import { useEffect, useState } from "react";
 
-export function useLocalStorageState<T>(
-  key: (typeof localStorageService.Key)[keyof typeof localStorageService.Key],
-  defaultValue: T
-): [T, (value: T) => void] {
+export function useLocalStorageState<T>({
+  defaultValue,
+  key,
+  onLoadData = (val) => val,
+}: {
+  key: (typeof localStorageService.Key)[keyof typeof localStorageService.Key];
+  defaultValue: T;
+  onLoadData?: (data: T) => T;
+}): [T, (value: T) => void] {
   const [state, setState] = useState<T>(defaultValue);
 
   // Hydration miss-match 를 막기 위해 useEffect 사용
   useEffect(() => {
-    setState(localStorageService.getItem<T>(key) ?? defaultValue);
+    const value = localStorageService.getItem<T>(key);
+    if (!value) {
+      setState(defaultValue);
+      return;
+    }
+
+    setState(onLoadData(value));
   }, []);
 
   const setItem = (value: T) => {
