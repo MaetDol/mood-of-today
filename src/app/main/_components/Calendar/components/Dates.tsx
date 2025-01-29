058a677f6209673a584e1e7b5c2449efa0da6f9e
MaetDol <rndka9@gmail.com>
2025-01-29 10:38:11 +0900

    feat, 그날의 무드 인디케이터 퍼블리싱 완료

--- src/app/main/_components/Calendar/components/Dates.tsx
+++ src/app/main/_components/Calendar/components/Dates.tsx
@@ -14,9 +14,33 @@ justify-items-center gap-x-6"
       {getDates(year, month).map(({ month: m, date }) => (
         <div
           key={m.toString() + date}
-          className={`mb-1 ${m !== month ? "opacity-20" : ""}`}
+          className={`mb-1 flex flex-col items-center ${
+            m !== month ? "opacity-20" : ""
+          }`}
         >
           {date}
+          <div className="rounded-[2px] overflow-hidden relative w-4 h-4">
+            <div
+              className="absolute rounded-full opacity-80 w-[18px] h-[18px]
+              left-[-8px] top-[-8px] bg-slate-100
+            "
+            />
+            <div
+              className="absolute rounded-full opacity-80 w-[18px] h-[18px]
+              left-[6px] top-[-6px] bg-slate-100
+            "
+            />
+            <div
+              className="absolute rounded-full opacity-80 w-[18px] h-[18px]
+              left-[-10px] top-[6px] bg-slate-100
+            "
+            />
+            <div
+              className="absolute rounded-full opacity-80 w-[18px] h-[18px]
+              left-[6px] top-[6px] bg-slate-100
+            "
+            />
+          </div>
         </div>
       ))}
     </div>
