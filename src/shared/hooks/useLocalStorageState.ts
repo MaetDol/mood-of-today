33c6277904b5d389fcc20c47501382d58c92f8b6
MaetDol <rndka9@gmail.com>
2025-01-29 09:53:19 +0900

    bugfix, hydration 에러 픽스
    client side 에서만 localStorage 를 쓰더라도, 최초 렌더링된 형태가 다르면 에러가 발생하는 것 같습니다.
    
    서버와 동일하게 최초 렌더링은 default value 로 처리하고, 다음 렌더링때 값을 재설정하게 수정했어요.

--- src/shared/hooks/useLocalStorageState.ts
+++ src/shared/hooks/useLocalStorageState.ts
@@ -1,15 +1,18 @@
 "use client";
 
 import { localStorageService } from "@/shared/services/localStorageService";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function useLocalStorageState<T>(
   key: (typeof localStorageService.Key)[keyof typeof localStorageService.Key],
   defaultValue: T
 ): [T, (value: T) => void] {
-  const [state, setState] = useState<T>(() => {
-    return localStorageService.getItem<T>(key) ?? defaultValue;
-  });
+  const [state, setState] = useState<T>(defaultValue);
+
+  // Hydration miss-match 를 막기 위해 useEffect 사용
+  useEffect(() => {
+    setState(localStorageService.getItem<T>(key) ?? defaultValue);
+  }, []);
 
   const setItem = (value: T) => {
     setState(value);
