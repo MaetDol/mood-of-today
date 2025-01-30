473082f04f3bd4daa15bd37b9a277c58e1f87758
MaetDol <rndka9@gmail.com>
2025-01-30 13:43:23 +0900

    feat, 기분 목록에 오늘것만 나오게 수정

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -6,13 +6,13 @@ import { Moods } from "@/app/main/_components/Moods";
 import { useMoodData } from "@/app/main/_hooks/useMoodData";
 
 export default function Page() {
-  const { addMood, moods } = useMoodData();
+  const { addMood, getMoodsByDate } = useMoodData();
 
   return (
     <main className="bg-white px-5 py-6 flex flex-col flex-1 max-h-screen">
       <div className="flex-1 shrink-0 flex flex-col gap-3 min-h-0">
         <MoodInput onCreateMood={(mood) => addMood(mood)} />
-        <Moods moods={moods} />
+        <Moods moods={getMoodsByDate(new Date())} />
       </div>
 
       <div className="mt-auto justify-center flex">
