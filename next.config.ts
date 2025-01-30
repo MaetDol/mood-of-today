94c643d138a99968a75c7cb879b7f13572ea80db
MaetDol <rndka9@gmail.com>
2025-01-30 23:24:03 +0900

    feat, PWA 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import pwa from "next-pwa";
 
-const nextConfig: NextConfig = {
+const withPWA = pwa({
+  dest: "public", // 컴파일된 Service Worker 파일을 저장할 위치
+  register: true, // 서비스 워커 자동 등록
+  skipWaiting: true, // 기존 워커 대기 없이 교체
+  disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = withPWA({
   /* config options here */
-};
+});
 
 export default nextConfig;
